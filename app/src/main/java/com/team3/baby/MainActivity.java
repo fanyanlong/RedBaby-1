a29b249ca1982b0aad2179ce63e7880fcfcc51e1
cuihansong <125769716@qq.com>
2017-05-16 14:50:37 +0800

    更新啦

--- app/src/main/java/com/team3/baby/MainActivity.java
+++ app/src/main/java/com/team3/baby/MainActivity.java
@@ -10,17 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //田杰谕去吃屎,大家都拍手叫好!!
-        //金虎前来报到
-        //崔涵淞篮子疼前去探菊花路被直男爆菊!!
-        //贾建海2017
-        //对赌丢丢丢敌对对独岛
-        //壮壮哥
-        //我是郭彦君。。。
-        //再来一次2017
-        //点击诶昂得分都不顾曾几何时动画
-        //啊啊啊啊啊啊啊啊
-        //aaaa
 
     }
 }
