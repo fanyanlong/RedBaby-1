73386f8a48b09f4ecb64372d5e1d724368dc197e
cuihansong <125769716@qq.com>
2017-05-17 21:08:03 +0800

    首页更新

--- app/src/main/java/com/team3/baby/module/fragments_home/adapter/HomeAdapter.java
+++ app/src/main/java/com/team3/baby/module/fragments_home/adapter/HomeAdapter.java
@@ -1,8 +1,10 @@
-package com.team3.baby.adapters;
+package com.team3.baby.module.fragments_home.adapter;
 
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * 类用途:
@@ -11,32 +13,37 @@ import android.view.ViewGroup;
  */
 
 public class HomeAdapter extends PagerAdapter {
+    private List<View> viewList;
+    private List<String> titleList;
 
-    public HomeAdapter() {
-
+    public HomeAdapter(List<View> viewList, List<String> titleList) {
+        this.viewList = viewList;
+        this.titleList = titleList;
     }
+
     @Override
     public int getCount() {
-        return 0;
+        return viewList.size();
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view == object;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        return super.instantiateItem(container, position);
+      // container.addView(viewList.get(position));
+        return viewList.get(position);
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        super.destroyItem(container, position, object);
+        container.removeView(viewList.get(position));
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return super.getPageTitle(position);
+        return titleList.get(position);//页卡标题;
     }
 }
