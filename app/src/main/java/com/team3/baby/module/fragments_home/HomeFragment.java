3d50b79e0065e27075b3d4887fd1fb6364493fea
cuihansong <125769716@qq.com>
2017-05-17 16:57:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/team3/baby/CuiHanSong_Activity.java

--- app/src/main/java/com/team3/baby/module/fragments_home/HomeFragment.java
+++ app/src/main/java/com/team3/baby/module/fragments_home/HomeFragment.java
@@ -1,8 +1,49 @@
 package com.team3.baby.module.fragments_home;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team3.baby.R;
+import com.team3.baby.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by tianjieyu on 2017/5/17.
  */
 
-public class HomeFragment {
+public class HomeFragment extends BaseFragment {
+    @BindView(R.id.tab_top_home_fragment)
+    TabLayout mTabTopHomeFragment;
+    @BindView(R.id.vp_home_fragment)
+    ViewPager mVpHomeFragment;
+
+    @Override
+    protected View initView() {
+        View view = View.inflate(mContext, R.layout.homefragment_layout, null);
+        return view;
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
 }
