f3f415117e0ea9baf656e96b355810cce00b9a97
jinhu1986 <78013593@qq.com>
2017-05-17 15:23:56 +0800

    jinhu  add BaseActivity,BaseFragment,App

---
+++ app/src/main/java/com/team3/baby/app/App.java
@@ -0,0 +1,44 @@
+package com.team3.baby.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * 类的用途：
+ * Created by jinhu
+ * 2017/5/17  15:10
+ */
+
+public class App {
+    private static App mAppApplication;
+
+    /**
+     * 单例模式
+     * 获取Application
+     *
+     * @return
+     */
+    public static App getApp() {
+        if (mAppApplication == null) {
+            synchronized (App.class) {
+                if (mAppApplication == null) {
+                    mAppApplication = new App();
+                }
+            }
+        }
+        return mAppApplication;
+    }
+
+    /**
+     * 获得全局的shareperence
+     *
+     * @return
+     */
+    public static SharedPreferences getSharedPrefernces(Context context) {
+        context = context.getApplicationContext();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("config", MODE_PRIVATE);
+        return sharedPreferences;
+    }
+}
--- app/src/main/java/com/team3/baby/base/BaseActivity.java
+++ app/src/main/java/com/team3/baby/base/BaseActivity.java
@@ -1,10 +1,61 @@
 package com.team3.baby.base;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team3.baby.app.App;
+
+import butterknife.ButterKnife;
+
 /**
  * 类的用途：
  * Created by jinhu
  * 2017/5/17  14:44
  */
 
-public abstract class BaseActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+    protected Activity mActivity;
+    protected SharedPreferences mSharedPreferences;
+    protected SharedPreferences.Editor mEditor;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        //常用属性
+        mActivity = this;
+        mSharedPreferences = App.getSharedPrefernces(mActivity);
+        mEditor = mSharedPreferences.edit();
+        ButterKnife.bind(this);
+        //初始化视图控件
+        initView();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //初始化数据
+        initData();
+        //获取网络数据
+        getServerData();
+        //判断登录状态,登录后的操作
+        //initLoged(loged());
+        //设置监听
+        setListener();
+        //其他
+        //ortherToDo();
+    }
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    protected abstract void getServerData();
+
+    //protected abstract void initLoged(boolean isloged);
+
+    protected abstract void setListener();
+
+    //protected abstract void ortherToDo();
 }
