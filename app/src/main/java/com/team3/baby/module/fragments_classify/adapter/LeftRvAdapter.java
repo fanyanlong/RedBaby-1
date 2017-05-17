4fcf164dcafde36de1b9fa16e2d79259cbd084bc
jinhu1986 <78013593@qq.com>
2017-05-17 18:29:56 +0800

    jinhu  add FragmentClassify

--- app/src/main/java/com/team3/baby/module/fragments_classify/adapter/LeftRvAdapter.java
+++ app/src/main/java/com/team3/baby/module/fragments_classify/adapter/LeftRvAdapter.java
@@ -1,7 +1,14 @@
 package com.team3.baby.module.fragments_classify.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.team3.baby.R;
+
+import java.util.List;
 
 /**
  * 类的用途：
@@ -9,20 +16,39 @@ import android.view.ViewGroup;
  * 2017/5/17  16:53
  */
 
-public class LeftRvAdapter extends RecyclerView.Adapter {
+public class LeftRvAdapter extends RecyclerView.Adapter<LeftRvAdapter.ViewHolder> {
+    private List<String> mList;
+    private Context mContext;
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public LeftRvAdapter(List<String> list, Context context) {
+        mList = list;
+        mContext = context;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.tv_item_left_classify);
+        }
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = View.inflate(mContext, R.layout.item_left_classify, null);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String s = mList.get(position);
+        holder.mTextView.setText(s);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mList.size();
     }
 }
