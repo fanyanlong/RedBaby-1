23780904bf70919e30b1b256fdf447ce03ac8074
tianjieyu1029 <304308913@qq.com>
2017-05-17 14:16:06 +0800

    分包

--- app/src/main/java/com/team3/baby/modules/MainActivity.java
+++ app/src/main/java/com/team3/baby/modules/MainActivity.java
@@ -1,7 +1,9 @@
-package com.team3.baby;
+package com.team3.baby.modules;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.team3.baby.R;
 
 public class MainActivity extends AppCompatActivity {
 
