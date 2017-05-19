c5159ff1fcd40c639c9c43d44caba362f1c8513f
gyjguoyanjun <274616957@qq.com>
2017-05-19 15:23:59 +0800

    添加String类型

---
+++ app/src/main/java/com/team3/baby/module/fragments_shopping/Fragment_shopping_RecycleAdapter.java
@@ -0,0 +1,76 @@
+package com.team3.baby.module.fragments_shopping;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cundong.recyclerview.RecyclerViewUtils;
+import com.team3.baby.R;
+
+import java.util.ArrayList;
+
+/**
+ * data:2017/5/19
+ * author:郭彦君(Administrator)
+ * function:
+ */
+public class Fragment_shopping_RecycleAdapter extends RecyclerView.Adapter {
+
+    private Context context;
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<String> mDataList;
+    private RecyclerView fragmentShoppingRecyclerView;
+
+
+    public Fragment_shopping_RecycleAdapter(Context context, ArrayList<String> mDataList, RecyclerView fragmentShoppingRecyclerView) {
+        this.context = context;
+        this.mDataList = mDataList;
+        this.mLayoutInflater = LayoutInflater.from(context);
+        this.fragmentShoppingRecyclerView = fragmentShoppingRecyclerView;
+    }
+
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(mLayoutInflater.inflate(R.layout.sample_item_card, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        String item = mDataList.get(position);
+
+        ViewHolder viewHolder = (ViewHolder) holder;
+        viewHolder.textView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.info_text);
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String text = mDataList.get(RecyclerViewUtils.getAdapterPosition(fragmentShoppingRecyclerView, ViewHolder.this));
+                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+}
