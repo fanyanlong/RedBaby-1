03b02232660c8a28f258adc17911a362aac5b8fb
gyjguoyanjun <274616957@qq.com>
2017-05-23 14:58:33 +0800

    购物车添加数据

--- app/src/main/java/com/team3/baby/module/fragments_shopping/AddIndentActivity.java
+++ app/src/main/java/com/team3/baby/module/fragments_shopping/AddIndentActivity.java
@@ -1,20 +1,51 @@
 package com.team3.baby.module.fragments_shopping;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.team3.baby.R;
+import com.team3.baby.module.fragments_shopping.shoppingutils.Shop_Utils;
+import com.team3.baby.utils.ImageUtils;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AddIndentActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv_add_picture)
+    ImageView ivAddPicture;
+    @BindView(R.id.tv_add_shoppingName)
+    TextView tvAddShoppingName;
+    @BindView(R.id.tv_add_shoppingPrice)
+    TextView tvAddShoppingPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_add_indent);
+        ButterKnife.bind(this);
+        initData();
+
+    }
+
+    private void initData() {
+        ArrayList<String> picUrl = Shop_Utils.getPicUrl();
+        Intent intent = getIntent();
+        int position = intent.getIntExtra("position", 0);
+        String shopName = intent.getStringExtra("shopName");
+        String shopPrice = intent.getStringExtra("shopPrice");
+        ImageUtils.loadImageNormal(this,picUrl.get(position),ivAddPicture);
+        tvAddShoppingName.setText(shopName);
+        tvAddShoppingPrice.setText(shopPrice);
 
 
     }
