e8843ebc653114f51f553f96bc812d1f3b0ee583
gyjguoyanjun <274616957@qq.com>
2017-05-23 11:34:10 +0800

    购物车添加数据

--- app/src/main/java/com/team3/baby/module/fragments_shopping/Fragment_shopping_RecycleAdapter.java
+++ app/src/main/java/com/team3/baby/module/fragments_shopping/Fragment_shopping_RecycleAdapter.java
@@ -6,11 +6,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.cundong.recyclerview.RecyclerViewUtils;
+import com.bumptech.glide.Glide;
 import com.team3.baby.R;
+import com.team3.baby.module.fragments_shopping.shopping_bean.Shopping_Bean;
 
 import java.util.ArrayList;
 
@@ -23,11 +24,11 @@ public class Fragment_shopping_RecycleAdapter extends RecyclerView.Adapter {
 
     private Context context;
     private LayoutInflater mLayoutInflater;
-    private ArrayList<String> mDataList;
+    private ArrayList<Shopping_Bean.GoodsBean> mDataList;
     private RecyclerView fragmentShoppingRecyclerView;
 
 
-    public Fragment_shopping_RecycleAdapter(Context context, ArrayList<String> mDataList, RecyclerView fragmentShoppingRecyclerView) {
+    public Fragment_shopping_RecycleAdapter(Context context, ArrayList<Shopping_Bean.GoodsBean> mDataList, RecyclerView fragmentShoppingRecyclerView) {
         this.context = context;
         this.mDataList = mDataList;
         this.mLayoutInflater = LayoutInflater.from(context);
@@ -43,10 +44,14 @@ public class Fragment_shopping_RecycleAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-        String item = mDataList.get(position);
-
         ViewHolder viewHolder = (ViewHolder) holder;
-        viewHolder.textView.setText(item);
+        viewHolder.textView.setText(mDataList.get(position).getAuxdescription());
+        viewHolder.price_text.setText(mDataList.get(position).getPrice());
+        viewHolder.tv_manypeoplebuy.setText(mDataList.get(position).getExtenalFileds().getCommentShow());
+        Glide
+                .with(context)
+                .load("http://image2.suning.cn/uimg/cms/img/149087236896225414.png")
+                .into(viewHolder.imageView);
     }
 
     @Override
@@ -57,17 +62,21 @@ public class Fragment_shopping_RecycleAdapter extends RecyclerView.Adapter {
     private class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView textView;
+        private final ImageView imageView;
+        private final TextView price_text;
+        private final TextView tv_manypeoplebuy;
 
         public ViewHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.info_text);
+            imageView = (ImageView) itemView.findViewById(R.id.img_shopping);
+            price_text = (TextView) itemView.findViewById(R.id.price_text);
+            tv_manypeoplebuy = (TextView) itemView.findViewById(R.id.tv_manypeoplebuy);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String text = mDataList.get(RecyclerViewUtils.getAdapterPosition(fragmentShoppingRecyclerView, ViewHolder.this));
-                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(context,ShoppingCarActivity.class);
+                    Intent intent = new Intent(context, ShoppingCarActivity.class);
                     context.startActivity(intent);
                 }
             });
