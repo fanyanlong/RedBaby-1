2eb9f712b8f3c4e599813bb72e5bb2d65de64848
gyjguoyanjun <274616957@qq.com>
2017-05-23 17:08:09 +0800

    购物车添加数据

--- app/src/main/java/com/team3/baby/module/fragments_shopping/AddIndent_buyActivity.java
+++ app/src/main/java/com/team3/baby/module/fragments_shopping/AddIndent_buyActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.team3.baby.R;
+import com.team3.baby.module.fragments_shopping.indent_activity.IndentAffirmActivity;
 import com.team3.baby.module.fragments_shopping.shoppingutils.Shop_Utils;
 import com.team3.baby.utils.ImageUtils;
 
@@ -18,7 +21,7 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AddIndentActivity extends AppCompatActivity {
+public class AddIndent_buyActivity extends AppCompatActivity {
 
     @BindView(R.id.iv_add_picture)
     ImageView ivAddPicture;
@@ -26,6 +29,8 @@ public class AddIndentActivity extends AppCompatActivity {
     TextView tvAddShoppingName;
     @BindView(R.id.tv_add_shoppingPrice)
     TextView tvAddShoppingPrice;
+    @BindView(R.id.btn_confirm_buy)
+    Button btnConfirmBuy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +44,24 @@ public class AddIndentActivity extends AppCompatActivity {
 
     private void initData() {
         ArrayList<String> picUrl = Shop_Utils.getPicUrl();
-        Intent intent = getIntent();
-        int position = intent.getIntExtra("position", 0);
-        String shopName = intent.getStringExtra("shopName");
-        String shopPrice = intent.getStringExtra("shopPrice");
-        ImageUtils.loadImageNormal(this,picUrl.get(position),ivAddPicture);
+        final Intent intent = getIntent();
+        final int position = intent.getIntExtra("position", 0);
+        final String shopName = intent.getStringExtra("shopName");
+        final String shopPrice = intent.getStringExtra("shopPrice");
+        ImageUtils.loadImageNormal(this, picUrl.get(position), ivAddPicture);
         tvAddShoppingName.setText(shopName);
         tvAddShoppingPrice.setText(shopPrice);
 
-
+        btnConfirmBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(AddIndent_buyActivity.this, IndentAffirmActivity.class);
+                intent1.putExtra("position", position);
+                intent1.putExtra("shopName", shopName);
+                intent1.putExtra("shopPrice", shopPrice);
+                startActivity(intent1);
+            }
+        });
     }
 
     @Override
