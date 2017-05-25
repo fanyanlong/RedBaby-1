da55781fc802cf74063e20e7677918e9623605ea
cuihansong <125769716@qq.com>
2017-05-25 09:31:09 +0800

    首页

---
+++ app/src/main/java/com/team3/baby/module/fragments_home/inter_face/MyOnItemClickListener.java
@@ -0,0 +1,13 @@
+package com.team3.baby.module.fragments_home.inter_face;
+
+import android.view.View;
+
+/**
+ * 类用途:
+ * 作者:崔涵淞
+ * 时间: 2017/5/24 17:00.
+ */
+
+public interface MyOnItemClickListener {
+    void OnItemClickListener(View view, int position);
+}
--- app/src/main/java/com/team3/baby/module/fragments_home/url/Url.java
+++ app/src/main/java/com/team3/baby/module/fragments_home/url/Url.java
@@ -9,4 +9,6 @@ package com.team3.baby.module.fragments_home.url;
 public class Url {
     public static final String TITLE = "http://lib.suning.com/app/redbaby/80000_8.2.0-155.json";
     public static final String IMAGE ="http://image.suning.cn";
+    public static final String BABYDIAPERS = "lib.suning.com/api/app/babydiapers.json";
+    public static final String HTTP = "http://";
 }
