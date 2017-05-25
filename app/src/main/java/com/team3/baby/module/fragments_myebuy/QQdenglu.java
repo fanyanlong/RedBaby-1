1861f28161a3ff9c46227564bbd85e8fab2fd906
jerryjjh <290485788@qq.com>
2017-05-25 10:49:27 +0800

    20170525  10：45

--- app/src/main/java/com/team3/baby/module/fragments_myebuy/QQdenglu.java
+++ app/src/main/java/com/team3/baby/module/fragments_myebuy/QQdenglu.java
@@ -15,5 +15,6 @@ public class QQdenglu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qq_layout);
+        //qq
     }
 }
